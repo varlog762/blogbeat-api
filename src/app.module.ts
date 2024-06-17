e3577eca001d7e0541a360ae587031608b627be2
Gregory Pereverzev <boot762@gmail.com>
2024-06-17 18:38:08 +0300

    feat: add TagModule and TagController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TagModule } from './tag/tag.module';
 
 @Module({
-  imports: [],
+  imports: [TagModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/tag/tag.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+
+import { TagController } from './tag.controller';
+
+@Module({
+  imports: [],
+  controllers: [TagController],
+  providers: [],
+})
+export class TagModule {}
