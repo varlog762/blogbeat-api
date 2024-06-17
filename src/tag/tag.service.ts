37783f92a039e35c808733402207b7521d8295e2
Gregory Pereverzev <boot762@gmail.com>
2024-06-17 20:22:16 +0300

    feat: install and configure nodemon

--- src/tag/tag.service.ts
+++ src/tag/tag.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class TagService {
   findAll(): string[] {
-    return ['dragons', 'coffee'];
+    return ['dragons', 'coffee', 'apple'];
   }
 }
