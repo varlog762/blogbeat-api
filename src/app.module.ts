74cda321ef072f4aaf7443b2015d9155d4fd32e4
Gregory Pereverzev <boot762@gmail.com>
2024-06-18 18:12:52 +0300

    feat: install typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TagModule } from './tag/tag.module';
+import { AppController } from '@app/app.controller';
+import { AppService } from '@app/app.service';
+import { TagModule } from '@app/tag/tag.module';
 
 @Module({
   imports: [TagModule],
--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 
-import { TagService } from './tag.service';
+import { TagService } from '@app/tag/tag.service';
 
 @Controller('tags')
 export class TagController {
